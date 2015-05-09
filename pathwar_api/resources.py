1688ffabab68904059d6c51fa0c82d87618969a8
Manfred Touron <m@42.am>
2015-05-09 12:03:57 +0200

    WIP: switched to job field

--- pathwar_api/resources.py
+++ pathwar_api/resources.py
@@ -2018,9 +2018,15 @@ tasks = {
     'allowed_item_write_roles': ['admin'],
 
     'schema': {
-        'name': {
+        'job': {
             'type': 'string',
             'required': True,
+        },
+        'args': {
+            'type': 'list',
+        },
+        'duration': {
+            'type': 'float',
         },
         'status': {
             'type': 'string',
